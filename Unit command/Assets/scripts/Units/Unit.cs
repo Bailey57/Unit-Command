d593302a89671d8a4c730d03b4fd332d73b1f816
Bailey57 <baileywilliams122@gmail.com>
2022-12-17 10:56:37 -0500

    Working on Unit movement

--- Unit command/Assets/scripts/Units/Unit.cs
+++ Unit command/Assets/scripts/Units/Unit.cs
@@ -5,68 +5,73 @@ using UnityEngine;
 
 
 
-namespace Assets 
-{ 
-public class Unit : MonoBehaviour
+namespace Assets
 {
-    [SerializeField] public string faction;
-    [SerializeField] private string unitName;
-    [SerializeField] private decimal moralLevel;
-    [SerializeField] private int soldierCount;
-    [SerializeField] private int vehivleCount;
-    [SerializeField] private decimal foodCount;
-    [SerializeField] private int ammoCount;
-    [SerializeField] private decimal fuelCount;
-    [SerializeField] private string order;
-    [SerializeField] private BoxCollider2D boxCollider;
+    public class Unit : MonoBehaviour
+    {
+        [SerializeField] public GameObject travelTarget;
+        [SerializeField] private float distanceToTarget;
+
+        [SerializeField] public string faction;
+        [SerializeField] private string unitName;
+        [SerializeField] private decimal moralLevel;
+        [SerializeField] private int soldierCount;
+        [SerializeField] private int vehivleCount;
+        [SerializeField] private decimal foodCount;
+        [SerializeField] private int ammoCount;
+        [SerializeField] private decimal fuelCount;
+        [SerializeField] private string order;
+
 
         //hours of combat experience the unit has combined
         //looses experience if it gains new recruits
-        [SerializeField] private BoxCollider2D experience;
-
-
-    
-
-    [SerializeField] private string speed;
+        [SerializeField] private decimal experience;
 
 
 
-        private bool EnemyInSight()
-    {
-        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size,
-            0, Vector2.positiveInfinity, 10);
-        return hit.collider != null;
-    }
+
+        [SerializeField] private float speed;
+        [SerializeField] private float viewDistance;
+
+        [SerializeField] private BoxCollider2D boxCollider;
 
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(boxCollider.bounds.center, boxCollider.bounds.size);
-    }
-
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (EnemyInSight()) 
+        public float GetDistance()
         {
-            //attack
+            return Vector2.Distance(transform.position, travelTarget.transform.position);
         }
-        
+        public Vector2 GetDirection()
+        {
+            return (travelTarget.transform.position - transform.position);
+        }
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //distanceToTarget = Vector2.Distance(transform.position, travelTarget.transform.position);
+            distanceToTarget = GetDistance();
+            Vector2 direction = GetDirection();
+            transform.position = Vector2.MoveTowards(this.transform.position, travelTarget.transform.position, speed * Time.deltaTime);
+            //if (EnemyInSight())
+            //{
+            //attack
+            //}
+
+
+
+        }
+
+
+
+
     }
-
-
-
-
-}
 
 
 }
